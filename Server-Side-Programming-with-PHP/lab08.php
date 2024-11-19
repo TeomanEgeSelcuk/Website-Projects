3e354288003b25875800b33f6d67ba809670edc0
Teoman Selcuk <tselcuk@scs.ryerson.ca>
2024-11-18 21:17:17 -0500

    Lab 8: Completed all tasks, including production mode testing for cookie expiration configuration. Verified background changes trigger hour updates.

--- Server-Side-Programming-with-PHP/lab08.php
+++ Server-Side-Programming-with-PHP/lab08.php
@@ -12,8 +12,29 @@ date_default_timezone_set('America/New_York');
 // Base directory for background images (We store our images in a folder called "bimages")
 $bg_dir = "bimages/";
 
+// ---------------------------------------------------------------------------
+// *** Cookie Expiration Configuration ***
+// Declare a single variable for cookie expiration in minutes.
+// Uncomment the appropriate line depending on the environment.
+// ---------------------------------------------------------------------------
+
+$expiry_minutes = 1440; // Production: Cookie expires after 24 hours
+// $expiry_minutes = 1;    // Testing: Cookie expires after 1 minute
+
+$expiry_seconds = $expiry_minutes * 60; // Convert minutes to seconds
+
+// ---------------------------------------------------------------------------
+// End of Cookie Expiration Configuration
+// ---------------------------------------------------------------------------
+
 // Problem 1: Time-based greeting and background image
 $hour = date('H'); // Get the current hour of the day
+
+// Uncomment one of the lines below to test different time periods:
+// $hour = 8;   // Test Morning (6 AM - 11:59 AM)
+// $hour = 14;  // Test Afternoon (12 PM - 5:59 PM)
+// $hour = 19;  // Test Evening (6 PM - 8:59 PM)
+// $hour = 22;  // Test Night (9 PM - 5:59 AM)
 
 // Determine greeting and background image based on the current hour
 // Depending on the time of day, the greeting and background image will change
@@ -56,13 +77,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['rows'], $_GET['columns'
 // Problem 3: Favorite image selection
 if (isset($_POST['favorite_image'])) {
     $favorite_image = $_POST['favorite_image'];  // Get the selected favorite image from the form
-    setcookie('favorite_image', $favorite_image, time() + 86400);  // Save the selected favorite image in a cookie (expires in 24 hours)
+    setcookie('favorite_image', $favorite_image, time() + $expiry_seconds);  // Save the selected favorite image in a cookie
+    setcookie('favorite_image_set_time', time(), time() + $expiry_seconds);  // Save the current timestamp when the cookie is set
     header("Location: lab08.php");  // Reload the page to apply the changes
     exit;  // Stop the script execution here to avoid further processing
 }
 
 // Check if the favorite image is set (from the cookie), if so, get the value
 $favorite_image = isset($_COOKIE['favorite_image']) ? $_COOKIE['favorite_image'] : null;
+
+// Check the time elapsed since the cookie was set
+if ($favorite_image && isset($_COOKIE['favorite_image_set_time'])) {
+    $set_time = intval($_COOKIE['favorite_image_set_time']);
+    $current_time = time();
+    $elapsed_seconds = $current_time - $set_time;
+
+    // Uncomment the following line to manually set elapsed seconds for testing
+    // $elapsed_seconds = 150;  // Example: Simulate that 150 seconds have passed since the cookie was set
+
+    if ($elapsed_seconds > $expiry_seconds) {
+        // Expire the favorite_image cookie
+        setcookie('favorite_image', '', time() - 3600);  // Set expiration time in the past
+        setcookie('favorite_image_set_time', '', time() - 3600);  // Expire the set_time cookie as well
+        $favorite_image = null;  // Remove the favorite image from the current session
+    }
+}
 
 ?>
 
